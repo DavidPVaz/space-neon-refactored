fd6055a3de79095e09fe6616260bfe772f4a4c7c
David Vaz <davidzav13@gmail.com>
2019-12-12 11:12:43 +0000

    moved the inputs setup to game screen show, and extracted common behaviour of player lives generation

--- src/david/vaz/space/neon/refactored/screen/game/GameScreen.java
+++ src/david/vaz/space/neon/refactored/screen/game/GameScreen.java
@@ -32,11 +32,14 @@ public final class GameScreen extends AbstractScreen {
 
     @Override
     public void setup() {
-        setupInputs();
+        // input setup need to be invoked before show,
+        // since this screen work for both single and multi-player,
+        // I need the engine state to build just one, or two players
     }
 
     @Override
     public void show() {
+        setupInputs();
         super.show();
         topBar.show();
         bottomBar.show();
@@ -54,25 +57,17 @@ public final class GameScreen extends AbstractScreen {
 
     private void setupPlayers() {
 
-        Stack<LifeIcon> playerOneLifeList = new Stack<>();
+        Stack<LifeIcon> playerOneLives = buildPlayerLives(PLAYER_ONE_LIFE_X, PLAYER_ONE_LIVES_MARGIN, LifeIcon.Type.BLUE);
 
-        for (int i = 0; i < PLAYERS_MAX_LIVES; i++) {
-            playerOneLifeList.push(new LifeIcon(PLAYER_ONE_LIFE_X + i * PLAYER_ONE_LIVES_MARGIN, LIFE_ICON_Y, LifeIcon.Type.BLUE));
-        }
-
-        playerOne = new Player(PLAYER_ONE_INITIAL_X, PLAYERS_INITIAL_Y, Image.PLAYER_BLUE, Bullet.Type.BLUE, playerOneLifeList);
+        playerOne = new Player(PLAYER_ONE_INITIAL_X, PLAYERS_INITIAL_Y, Image.PLAYER_BLUE, Bullet.Type.BLUE, playerOneLives);
 
         if (!getEngine().getActiveState().equals(Engine.State.MULTI_PLAYER_GAME)) {
             return;
         }
 
-        Stack<LifeIcon> playerTwoLifeList = new Stack<>();
+        Stack<LifeIcon> playerTwoLives = buildPlayerLives(PLAYER_TWO_LIFE_X, PLAYER_TWO_LIVES_MARGIN, LifeIcon.Type.GREEN);
 
-        for (int i = 0; i < PLAYERS_MAX_LIVES; i++) {
-            playerTwoLifeList.push(new LifeIcon(PLAYER_TWO_LIFE_X + i * PLAYER_TWO_LIVES_MARGIN, LIFE_ICON_Y, LifeIcon.Type.GREEN));
-        }
-
-        playerTwo = new Player(PLAYER_TWO_INITIAL_X, PLAYERS_INITIAL_Y, Image.PLAYER_GREEN, Bullet.Type.GREEN, playerTwoLifeList);
+        playerTwo = new Player(PLAYER_TWO_INITIAL_X, PLAYERS_INITIAL_Y, Image.PLAYER_GREEN, Bullet.Type.GREEN, playerTwoLives);
     }
 
     private void setupInputs() {
@@ -90,6 +85,10 @@ public final class GameScreen extends AbstractScreen {
         addInputHandler(Key.SPACE, Input.Type.KEY_PRESS, () -> playerOne.fire());
         addInputHandler(Key.SPACE, Input.Type.KEY_RELEASE, () -> playerOne.stopFiring());
 
+        if (!getEngine().getActiveState().equals(Engine.State.MULTI_PLAYER_GAME)) {
+            return;
+        }
+
         addInputHandler(Key.D, Input.Type.KEY_PRESS, () -> playerTwo.addDirection(Direction.EAST));
         addInputHandler(Key.A, Input.Type.KEY_PRESS, () -> playerTwo.addDirection(Direction.WEST));
         addInputHandler(Key.W, Input.Type.KEY_PRESS, () -> playerTwo.addDirection(Direction.NORTH));
@@ -102,6 +101,16 @@ public final class GameScreen extends AbstractScreen {
 
         addInputHandler(Key.T, Input.Type.KEY_PRESS, () -> playerTwo.fire());
         addInputHandler(Key.T, Input.Type.KEY_RELEASE, () -> playerTwo.stopFiring());
+    }
 
+    private Stack<LifeIcon> buildPlayerLives(double lifeInitialX, double livesMargin, LifeIcon.Type type) {
+
+        Stack<LifeIcon> livesStack = new Stack<>();
+
+        for (int i = 0; i < PLAYERS_MAX_LIVES; i++) {
+            livesStack.push(new LifeIcon(lifeInitialX + i * livesMargin, LIFE_ICON_Y, type));
+        }
+
+        return livesStack;
     }
 }
